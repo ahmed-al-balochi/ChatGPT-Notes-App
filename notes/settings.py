890ba9d2f3829fd20ef193120e2f487e8c68a090
Ahmed Al Balochi <a7mad98.work@gmail.com>
2023-03-09 15:53:51 +0500

    changing deployment settings

--- notes/settings.py
+++ notes/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 
     "corsheaders.middleware.CorsMiddleware",
-
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
