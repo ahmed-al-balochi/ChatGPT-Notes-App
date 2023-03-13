9cc28144f8b9b1f11e0f463584a1432cbf5ffb66
Ahmed Al Balochi <a7mad98.work@gmail.com>
2023-03-13 18:59:04 +0500

    added rtl support for Arabic

--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+const LanguageDetect = require('languagedetect');
 
 let createdAt = (note) => {
   return new  Date(note.created_at).toLocaleDateString()
@@ -12,17 +13,29 @@ let modifiedAt = (note) => {
 
 let getContent = (note) => {
   let content = note.content
-  if(content.length > 45) {
-    return content.slice(0,45) + '...'
+  if(content.length > 55) {
+    return content.slice(0,55) + '...'
   } else {
     return content
+  }
+}
+
+function textContentDirection(dir){
+  const lngDetector = new LanguageDetect();
+  let text = " "
+  text = lngDetector.detect(dir)
+  //console.log(text)
+  if(0 !== text.length && 'arabic' === text[0][0]){
+    return 'rtl' 
+  }else{
+    return 'ltr'
   }
 }
 
 const ListItem = ({note}) => {
   return (
     <Link to={`/notes/${note.id}`}>
-      <div className='notes-list-item'>
+      <div dir={textContentDirection(note.content)} className='notes-list-item'>
         <h2>{note.title}</h2>
         <p><span>Created: {createdAt(note)} | Updated: {modifiedAt(note)}</span></p>
         <p><span>{getContent (note)}</span></p>
